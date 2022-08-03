2e461eec264e1d9dd73f5a5956a887aeda83579f
Thomas Pendleton <thomas6576@yahoo.com>
2022-08-02 22:34:21 -0700

    html edit

--- src/components/GameDirection/GameDirection.js
+++ src/components/GameDirection/GameDirection.js
@@ -7,16 +7,16 @@ const GameDirection = () => {
       <ol className="directionItems">
         <li>
           Click on settings button at top to change player name, time, and math
-          questions
+          questions.
         </li>
-        <li>When Ready, click the Play Now button at top left</li>
+        <li>When Ready, click the Play Now button at top left.</li>
         <li>
           Your score will increment and give you a total score of the correct
-          answers minus the wrong answers
+          answers minus the wrong answers.
         </li>
         <li>
           You can see how well you have done by clicking the high scores button
-          at the top right
+          at the top right.
         </li>
       </ol>
     </div>
