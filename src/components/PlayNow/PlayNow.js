f27b869d8120550c551d81d2ea4205d5ab8e6783
Thomas Pendleton <thomas6576@yahoo.com>
2022-09-01 09:28:33 -0700

    minor change to play now css

--- src/components/PlayNow/PlayNow.js
+++ src/components/PlayNow/PlayNow.js
@@ -1,22 +1,21 @@
-import React from 'react'
-import './PlayNow.style.css'
+import React from "react";
+import "./PlayNow.style.css";
 
 const PlayNow = ({ firstNumber, secondNumber }) => {
   return (
-   
-      <div className="questionContainer">
-        <section className="question">
-          <h4 className="firstNumber">{firstNumber}</h4>
-          <h4 className="secondNumber">
-            <span className="operation">X</span>
-            {secondNumber}
-          </h4>
-        </section>
-        <div className="line"></div>
-        <input className="answer" type="number" onSubmit={() => {}} />
-      </div>
-  
-  )
-}
+    <div className="questionContainer">
+      <section className="question">
+        <h4 className="firstNumber">{firstNumber}</h4>
+        <h4 className="secondNumber">
+          <span className="operation">X</span>
+          {secondNumber}
+        </h4>
+      </section>
+      <div className="line"></div>
 
-export default PlayNow
+      <input className="answer" max="999" type="number" onSubmit={() => {}} />
+    </div>
+  );
+};
+
+export default PlayNow;
