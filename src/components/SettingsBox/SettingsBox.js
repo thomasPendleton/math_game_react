6ff8b27f733adbc47b5d66e0465412bce30e2f60
Thomas Pendleton <thomas6576@yahoo.com>
2022-09-10 17:06:19 -0700

    remove css import for settings box

--- src/components/SettingsBox/SettingsBox.js
+++ src/components/SettingsBox/SettingsBox.js
@@ -2,7 +2,6 @@ import React, { useState, useRef, useEffect } from "react"
 import styled from "styled-components"
 import { GameContext } from "../../context/GameContext"
 
-import "./SettingsBox.style.css"
 
 const SettingsBox = ({ handleTabChange }) => {
   const { setPlayerName, playerName, setGameTime, setOperation } =
