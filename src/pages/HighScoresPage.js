7d0a6cf774f8fb8fd868c1cf516b1ad394fbd6d6
Thomas Pendleton <thomas6576@yahoo.com>
2022-12-23 20:52:59 -0800

    add animation for loading

--- src/pages/HighScoresPage.js
+++ src/pages/HighScoresPage.js
@@ -55,6 +55,12 @@ const Wrapper = styled.main`
     padding: 10px 0;
     background-color: #ffffff;
     background-image: url("data:image/svg+xml,%3Csvg width='84' height='48' viewBox='0 0 84 48' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M0 0h12v6H0V0zm28 8h12v6H28V8zm14-8h12v6H42V0zm14 0h12v6H56V0zm0 8h12v6H56V8zM42 8h12v6H42V8zm0 16h12v6H42v-6zm14-8h12v6H56v-6zm14 0h12v6H70v-6zm0-16h12v6H70V0zM28 32h12v6H28v-6zM14 16h12v6H14v-6zM0 24h12v6H0v-6zm0 8h12v6H0v-6zm14 0h12v6H14v-6zm14 8h12v6H28v-6zm-14 0h12v6H14v-6zm28 0h12v6H42v-6zm14-8h12v6H56v-6zm0-8h12v6H56v-6zm14 8h12v6H70v-6zm0 8h12v6H70v-6zM14 24h12v6H14v-6zm14-8h12v6H28v-6zM14 8h12v6H14V8zM0 8h12v6H0V8z' fill='%236ffc00' fill-opacity='0.18' fill-rule='evenodd'/%3E%3C/svg%3E");
+    h1 {
+      animation: fade 1s ease-in;
+      animation-iteration-count: infinite;
+      animation-direction: alternate;
+      opacity: 0.1;
+    }
   }
   h1 {
     margin: 0;
@@ -63,13 +69,21 @@ const Wrapper = styled.main`
     text-align: center;
   }
   @media only screen and (min-width: 500px) {
-    h1{
+    h1 {
       margin: 20px 0;
     }
     .container {
       margin: 0 auto;
       width: 90%;
       max-width: 550px;
+    }
+  }
+  @keyframes fade {
+    0% {
+      opacity: 0.1;
+    }
+    100% {
+      opacity: 1;
     }
   }
 `
